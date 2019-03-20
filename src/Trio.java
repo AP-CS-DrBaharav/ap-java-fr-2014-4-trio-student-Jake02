0913850efcdb56745425b0e93c87351e0a6c828e
ZachiBaharav <zachi@baharav.org>
2019-03-20 11:03:01 -0700

    Init.

---
+++ src/Trio.java
@@ -0,0 +1,23 @@
+
+/*
+
+You should have your code here.
+For proper testing, please include the below import at the top of your file.
+
+
+import java.text.DecimalFormat;
+
+
+
+Then, please include the below toString() at the bottom of your class.
+
+  public String toString ()
+  {
+    DecimalFormat money = new DecimalFormat("0.00");
+    return getName() + " " + money.format(getPrice());
+  }  
+
+
+********* Those were NOT required in the test, and are merely a convinence for the cheking.
+
+*/
